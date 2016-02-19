94c0615382cc8ee7acc3d20686e8b04f8553dcd5
Jens Kraemer <jk@jkraemer.net>
2016-02-19 13:42:01 +0100

    do not enforce secure cookies unconditionally, remove code duplication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,4 @@
 # along with Bold.  If not, see <http://www.gnu.org/licenses/>.
 #
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_bold_session', secure: Rails.env.production?
+# we do not secure the session cookie here so we can still publish non-ssl
+# sites with a session (which is necessary even for the simplest comment form
+# due to csrf protection). Secure cookies instead should be enforced per hostname by
+# a middleware, i.e. the rack-tls-tools lib which I use:
+# https://github.com/jkraemer/rack-tls_tools
+Rails.application.config.session_store :cookie_store, key: '_session'
